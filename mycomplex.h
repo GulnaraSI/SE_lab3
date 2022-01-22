7220221f62d5979125a94451ca58b1f0d0cac470
Gulnara Sibagatullina <googlya7@yandex.ru>
2022-01-23 00:54:15 +0500

    Vneseny vse comment

--- mycomplex.h
+++ mycomplex.h
@@ -17,27 +17,26 @@ public:							//Spisok chlenov klassov
 void Set(double aRe, double aIm = 0);			//Ustanovka deystvitelnoy i mnimoy chacti
 operator double();					//Vozvraschaet modul' kompleksnogo chisla
 double abs();						//Modul' koplesnogo chisla
-friend istream& operator >> (istream & , Complex &) ; 	// Druzhestvennyi mrtod operacii perezagruzki vvoda
-friend ostream& operator << (ostream & , Complex & );	// Druzhestvennyi mrtod operacii perezagruzki vyvoda
-Complex operator + (const Complex &);
-Complex operator - (const Complex &);
-Complex operator + (const double &);
-Complex operator - (const double &);
-Complex operator * (const Complex &);
-Complex operator * (const double &);
-friend 		Complex operator+ (const double&, const Complex&);
-
-friend Complex operator- (const 	double&, const Complex&);
-
-
-friend Complex operator* (const double&, const Complex&)    ;Complex operator/ (const double&);
-
-    Complex& operator+= (const Complex&);
-Complex  &  operator-= (const Complex&)    ;
-    Complex& operator*= (const Complex& );  Complex& operator  += (const double&);
-Complex& operator-= (const double&);
-Complex  &operator  *=(const double  & );
-Complex  & operator /=( const   double & );
-Complex& operator = (  const   Complex&);
-Complex & operator= (const double&);};
+friend istream& operator >> (istream & , Complex &) ; 	// Druzhestvennyi metod operacii perezagruzki vvoda
+friend ostream& operator << (ostream & , Complex & );	// Druzhestvennyi metod operacii perezagruzki vyvoda
+Complex operator + (const Complex &);			//Operaciya slozheniya mnimoy chasti
+Complex operator - (const Complex &);			//Operaciya vychitaniya mnimoy chasti
+Complex operator + (const double &);			//Operaciya slozheniya deystvitelnogo chasti
+Complex operator - (const double &);			//Operaciya vychitaniya deystvitelnogo chasti
+Complex operator * (const Complex &);			//Operaciya umnozheniya mnimoy chasti
+Complex operator * (const double &);			//Operaciya umnozheniya deystvitelnogo chasti
+Complex operator / (const double &);			//Operaciya deleniya deystvitelnogo chasti
+friend Complex operator + (const double & , const Complex &);		//Druzhestvennyi metod operacii slozheniya
+friend Complex operator - (const double & , const Complex &);		//Druzhestvennyi metod operacii vychitaniya
+friend Complex operator * (const double & , const Complex &);		//Druzhestvennyi metod operacii umnozheniya
+Complex & operator += (const Complex &);				//Perezagruzka operaciya slozheniya kopleksnogo chisla
+Complex & operator -= (const Complex &);				//Perezagruzka operaciya vychitaniya kopleksnogo chisla
+Complex & operator *= (const Complex &); 				//Perezagruzka operaciya umnozheniya kopleksnogo chisla
+Complex & operator += (const double &);					//Perezagruzka operaciya slozheniya deystvitelnogo chisla
+Complex & operator -= (const double &);					//Perezagruzka operaciya vychitaniya deystvitelnogo chisla
+Complex & operator *= (const double &);					//Perezagruzka operaciya umnozheniya deystvitelnogo chisla
+Complex & operator /= (const double &);					//Perezagruzka operaciya deleniya deystvitelnogo chisla
+Complex & operator = (const Complex &);					//Perezagruzka operaciya ravenstva kopleksnogo chisla
+Complex & operator = (const double &);					//Perezagruzka operaciya ravenstva deystvitelnogo chisla
+};
 #endif
