7f5e0a3c6f0d02a773f6baaca2dd1e8c1fd91f19
Gulnara Sibagatullina <googlya7@yandex.ru>
2022-01-23 00:39:00 +0500

    V prcesse redactirovaniya

--- mycomplex.h
+++ mycomplex.h
@@ -3,32 +3,33 @@
 #ifndef _MY_COMPLEX_H_
 #define _MY_COMPLEX_H_
 #include <iostream>
+
 using namespace std;
-class Complex
-{double Re;
-	double Im; 
-	public:
-Complex(double aRe=0, double aIm=0);
-Complex(const Complex&);
-~Complex(    );
-void Set(double aRe, double aIm=0);
 
-operator double();
-double abs();
-friend istream& operator>>(   istream&,Complex&) ; 
-
-
-
-friend ostream& operator<< (ostream&, Complex & );
-
-Complex operator+ (const Complex&);
-Complex operator- (const Complex&);
-Complex operator+ (const double&);
+class Complex  						//Klass kompleksnogo chisla
+{
+double Re;     						//Deistvitelnaya chast'
+double Im; 						//mnimaya chast'
+public:							//Spisok chlenov klassov
+	Complex(double aRe = 0, double aIm = 0);		//Konstruktor mnimoy i deystvitelnaoi chacti
+	Complex(const Complex&);			//Konstruktor kompleksnogo chisla
+~Complex(    );						//Destruktor
+void Set(double aRe, double aIm = 0);			//Ustanovka deystvitelnoy i mnimoy chacti
+operator double();					//Vozvraschaet modul' kompleksnogo chisla
+double abs();						//Modul' koplesnogo chisla
+friend istream& operator >> (istream & , Complex &) ; 	// Druzhestvennyi mrtod operacii perezagruzki vvoda
+friend ostream& operator << (ostream & , Complex & );	// Druzhestvennyi mrtod operacii perezagruzki vyvoda
+Complex operator + (const Complex &);
+Complex operator - (const Complex &);
+Complex operator + (const double &);
+Complex operator - (const double &);
+Complex operator * (const Complex &);
+Complex operator * (const double &);
 friend 		Complex operator+ (const double&, const Complex&);
-Complex operator- (const double&);
+
 friend Complex operator- (const 	double&, const Complex&);
-Complex operator* (const Complex&);
-Complex operator* (const double&);
+
+
 friend Complex operator* (const double&, const Complex&)    ;Complex operator/ (const double&);
 
     Complex& operator+= (const Complex&);
