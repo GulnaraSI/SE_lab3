321a0a5f6af246f9b676123e1c046a238abc4239
Gulnara Sibagatullina <googlya7@yandex.ru>
2022-01-23 22:56:17 +0500

    Added comments for Doxygen

--- mycomplex.cpp
+++ mycomplex.cpp
@@ -1,197 +1,309 @@
-/*Fail s opisaniem metodov klassa Complex*/
+/*!
+    @file
+    @brief Файл  с описанием методов класса Complex
+*/
 #include <iostream>
 #include <cmath>
 #include "mycomplex.h"
 
 using namespace std;
 
-Complex :: Complex (double aRe , double aIm)    //Konstruktor klassa
-{
-    Re = aRe;                                   //aRe - deystvitelnaya chact'
-    Im = aIm;                                   //aIm - mnimaya chact'
-}
-
-Complex :: Complex (const Complex & aRval)       //Konstruktor klassa
-{
-    Re = aRval.Re;                              //aRval - kompleksnoe chislo , aRval.Re - deystvitelnaya chact'
-    Im = aRval.Im;                              //aRval - kompleksnoe chislo , aRval.Im - mnimaya chact'
-}
-
-Complex :: ~Complex ()                          //Destruktor klassa
-{
-    Re = 0.0;
-    Im = 0.0;
-}
-
-void Complex :: Set (double aRe , double aIm)           //Ustanovka deystvitelnoy i mnimoy chacti
+/*!
+     @brief Конструктор класса
+     @param a_Re Действительная часть
+     @param a_im Мнимая часть
+*/
+Complex :: Complex (double aRe , double aIm)
 {
     Re = aRe;
     Im = aIm;
 }
 
-Complex :: operator double ()                           //Vozvraschaet modul' kompleksnogo chisla
+/*!
+     @brief Конструктор класса
+     @param a_Rval Комплексное число
+*/
+Complex :: Complex (const Complex & aRval)
 {
-    return abs ();                                      //Modul' koplesnogo chisla
+    Re = aRval.Re;
+    Im = aRval.Im;
 }
 
-double Complex :: abs ()                                //Vozvraschaet modul' vyrazheniya kompleksnogo chisla
+/*!
+     @brief Деструктор класса
+*/
+Complex :: ~Complex ()
 {
-return sqrt(Re * Re + Im * Im);                         //Modul' vyrazheniya koplesnogo chisla
+    Re = 0.0;
+    Im = 0.0;
 }
 
-Complex Complex :: operator + (const Complex & aRval)       //Vozvraschaet rezultat slozheniya kompleksnogo chisla
+/*!
+     @brief Установление мнимой и действительной частей
+*/
+void Complex :: Set (double aRe , double aIm)
+{
+    Re = aRe;
+    Im = aIm;
+}
+
+/*!
+     @brief Возвращает модуль комплексного числа
+     @return Модуль комплексное числа
+*/
+Complex :: operator double ()
+{
+    return abs ();
+}
+
+/*!
+     @brief Возвращает модуль выражения комплексного числа
+     @return Модуль выражения комплексного числа
+*/
+double Complex :: abs ()
+{
+return sqrt(Re * Re + Im * Im);
+}
+
+/*!
+     @brief Возвращает результат сложения комплексного числа
+     @return Результат сложения комплексного числа
+*/
+Complex Complex :: operator + (const Complex & aRval)
 {
     Complex Result;
     Result.Re = Re + aRval. Re;
     Result.Im = Im + aRval.Im;
-    return Result;                                          //Rezultat
+    return Result;
 }
 
-Complex Complex :: operator - (const Complex & aRval)           //Vozvraschaet rezultat vychitaniya kompleksnogo chisla
+/*!
+     @brief Возвращает результат вычитания комплексного числа
+     @return Результат сложения комплексного числа
+*/
+Complex Complex :: operator - (const Complex & aRval)
 {
     Complex Result;
     Result.Re = Re-aRval.Re;
     Result.Im = Im-aRval.Im;
-    return Result;                                              //Rezultat
+    return Result;
 }
 
-Complex Complex :: operator + (const double & aval)             //Vozvraschaet rezultat slozheniya deystvitelnogo chisla
+/*!
+     @brief Возвращает результат сложения действительного числа
+     @return Результат сложения комплексного числа
+*/
+Complex Complex :: operator + (const double & aval)
 {
     Complex result;
     result.Re = Re + aval;
     result.Im = Im;
-    return result;                                              //Rezultat
+    return result;
 }
 
-Complex Complex :: operator - (const  double & aRval)           //Vozvraschaet rezultat vychitaniya deystvitelnogo chisla
+/*!
+     @brief Возвращает результат вычитания действительного  числа
+     @return Результат
+*/
+Complex Complex :: operator - (const  double & aRval)
 {
     Complex Result(* this);
     Result.Re = Re - aRval;
-    return Result;                                              //Rezultat
+    return Result;
 }
 
-Complex Complex :: operator * (const Complex & aRval)           //Vozvraschaet rezultat umnozheniya kompleksnogo chisla
+/*!
+     @brief Возвращает результат умножения комплексного  числа
+     @return Результат
+*/
+Complex Complex :: operator * (const Complex & aRval)
 {
     Complex Result;
     Result.Re = Re * aRval.Re - Im * aRval.Im;
     Result.Im = Re * aRval.Im + Im * aRval.Re;
-    return Result;                                              //Rezultat
+    return Result;
 }
 
-Complex Complex :: operator * (const double & aRval)            //Vozvraschaet rezultat umnozheniya deystvitelnogo chisla
+/*!
+     @brief Возвращает результат умножения действительного  числа
+     @return Результат
+*/
+Complex Complex :: operator * (const double & aRval)
 {
     Complex Result;
     Result.Re = Re * aRval;
     Result.Im = Im * aRval;
-    return Result;                                              //Rezultat
+    return Result;
 }
-Complex Complex :: operator / (const double & aRval)            //Vozvraschaet rezultat deleniya deystvitelnogo chisla
+
+/*!
+     @brief Возвращает результат деления действительного  числа
+     @return Результат
+*/
+Complex Complex :: operator / (const double & aRval)
 {
     Complex Result;
     Result.Re = Re / aRval;
     Result.Im = Im / aRval;
-    return Result;                                              //Rezultat
+    return Result;
 }
 
-Complex & Complex :: operator += (const Complex & arval)        //Ssylaetcya na rezultat slozheniya kompleksnogo chisla
+/*!
+     @brief Ссылается на результат сложения комплексного  числа
+     @return возврат по значанию
+*/
+Complex & Complex :: operator += (const Complex & arval)
 {
     Re += arval.Re;
     Im += arval.Im;
-    return * this;                                              //Vozvrat po znacheniu
+    return * this;
 }
 
-Complex & Complex :: operator -= (const Complex & aRval)        //Ssylaetcya na rezultat vychitaniya kompleksnogo chisla
+/*!
+     @brief Ссылается на результат вычитания комплексного  числа
+     @return возврат по значанию
+*/
+Complex & Complex :: operator -= (const Complex & aRval)
 {
     Re -= aRval.Re;
     Im -= aRval.Im;
-    return * this;                                              //Vozvrat po znacheniu
+    return * this;
 }
 
-Complex & Complex :: operator *= (const Complex & aRval)        //Ssylaetcya na rezultat umnozheniya kompleksnogo chisla
+/*!
+     @brief Ссылается на результат умножения комплексного  числа
+     @return возврат по значанию
+*/
+Complex & Complex :: operator *= (const Complex & aRval)
 {
     double tmpRe = Re;
     Re = Re * aRval.Re - Im * aRval.Im;
     Im = Im * aRval.Re + tmpRe * aRval.Im;
-    return * this;                                              //Vozvrat po znacheniu
+    return * this;
 }
 
-Complex & Complex :: operator += (const double & aRval)         //Ssylaetcya na rezultat slozheniya deystvitelnogo chisla
+/*!
+     @brief Ссылается на результат сложения дейситвительного  числа
+     @return возврат по значанию
+*/
+Complex & Complex :: operator += (const double & aRval)
 {
     Re += aRval;
-    return * this;                                              //Vozvrat po znacheniu
+    return * this;
 }
 
-Complex & Complex :: operator -= (const double & aRval)         //Ssylaetcya na rezultat vychitaniya deystvitelnogo chisla
+/*!
+     @brief Ссылается на результат вычитания дейситвительного  числа
+     @return возврат по значанию
+*/
+Complex & Complex :: operator -= (const double & aRval)
 {
     Re -= aRval;
-    return * this;                                              //Vozvrat po znacheniu
+    return * this;
 }
 
-Complex & Complex :: operator *= (const double & aRval)         //Ssylaetcya na rezultat umnozheniya deystvitelnogo chisla
+/*!
+     @brief Ссылается на результат умножения действительного числа
+     @return возврат по значанию
+*/
+Complex & Complex :: operator *= (const double & aRval)
 {
     Re *= aRval;
     Im *= aRval;
-    return * this;                                              //Vozvrat po znacheniu
+    return * this;
 }
 
-Complex & Complex :: operator /= (const double & aRval)         //Ssylaetcya na rezultat deleniya deystvitelnogo chisla
+/*!
+     @brief Ссылается на результат деления действительного числа
+     @return возврат по значанию
+*/
+Complex & Complex :: operator /= (const double & aRval)
 {
     Re /= aRval;
     Im /= aRval;
-    return * this;                                              //Vozvrat po znacheniu
+    return * this;
 }
 
-Complex & Complex :: operator = (const Complex & aRval)         //Ssylaetcya na rezultat ravenstva kompleksnogo chisla
+/*!
+     @brief Ссылается на результат равенства комплексного числа
+     @return возврат по значанию
+*/
+Complex & Complex :: operator = (const Complex & aRval)
 {
     Re = aRval.Re;
     Im = aRval.Im;
-    return * this;                                              //Vozvrat po znacheniu
+    return * this;
 }
 
-Complex & Complex :: operator = (const double & aRval)          //Ssylaetcya na rezultat ravenstva deystvitelnogo chisla
+/*!
+     @brief Ссылается на результат равенства действительного числа
+     @return возврат по значанию
+*/
+Complex & Complex :: operator = (const double & aRval)
 {
     Re = aRval;
     Im = 0.0;
-    return * this;                                              //Vozvrat po znacheniu
+    return * this;
 }
 
-istream & operator >> (istream & stream , Complex & a)          //Izvlechenie deystvitelnoy i mnimoy chasti
+/*!
+     @brief Извлекаем мнимую и действительную часть
+     @return Извлечение
+*/
+istream & operator >> (istream & stream , Complex & a)
 {
     char tmp[256];
     stream >> a.Re >> a.Im >> tmp;
-    return stream;                                              //Izvlechenie
+    return stream;
 }
 
-ostream & operator << (ostream & stream , Complex & a)          //Vstavlyaem deystvitelnuu i mnimuu chasti
+/*!
+     @brief Вставляем мнимую и действительную часть
+     @return Вставка
+*/
+ostream & operator << (ostream & stream , Complex & a)
 {
     stream << a.Re;
     if( ! (a.Im < 0) )
         stream << '+';
     stream << a.Im << 'i';
-    return stream;                                              //Vstavka
+    return stream;
 }
 
-Complex operator + (const double & aLval , const Complex & aRval)       //Vozvraschaet rezultat slozheniya deystvitelnogo i komplesnogo chisla
+/*!
+     @brief Возвращает результат сложения действительного и комплексного числа
+     @param aLval Действительное число
+     @return Результат сложения
+*/
+Complex operator + (const double & aLval , const Complex & aRval)
 {
     Complex Result;
-    Result.Re = aLval + aRval.Re;                                       //aLval - deystvitelnoe chislo
+    Result.Re = aLval + aRval.Re;
     Result.Im = aRval.Im;
-    return Result;                                                      //Rezultat
+    return Result;
 }
 
-Complex operator - (const double & aLval , const Complex & aRval)       //Vozvraschaet rezultat vychitaniya deystvitelnogo i komplesnogo chisla
+/*!
+     @brief Возвращает результат вычитания действительного и комплексного числа
+     @return Результат вычитания
+*/
+Complex operator - (const double & aLval , const Complex & aRval)
 {
     Complex Result;
     Result.Re = aLval - aRval.Re;
     Result.Im =- aRval.Im;
-    return Result;                                                      //Rezultat
+    return Result;
 }
 
-Complex operator * (const double & aLval , const Complex & a)           //Vozvraschaet rezultat umnozheniya deystvitelnogo i komplesnogo chisla
+/*!
+     @brief Возвращает результат умножения действительного и комплексного числа
+     @return Результат умножения
+*/
+Complex operator * (const double & aLval , const Complex & a)
 {
     Complex r;
     r.Re = aLval * a.Re;
     r.Im = aLval * a.Im;
-    return r;                                                           //Rezultat
+    return r;
 }
 
